ff0df0c2b3600329e70fdddb899c19137096adc2
Chamath Anjula <124298134+chamathAn@users.noreply.github.com>
2024-07-08 13:01:24 +0530

    funtions, classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
 import hello from './hello';
+let abc: string = 'hello';
+abc = "fdsf";
+const abc1: string = 'hello2';
+let obj : object = {};
 
-hello();
+class Cat{
+
+    static className: string = 'Cat';
+    name: string;
+    constructor(name: string){
+        this.name=name;
+    }
+
+    static printClassName(){
+        console.log(Cat.className
+        );
+    }
+        
+    printName(){
+        console.log(this.name);
+    }
+}
+
+
+const c = new Cat("Garfield")
+
+c.printName();
+Cat.printClassName();
